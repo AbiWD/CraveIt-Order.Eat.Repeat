21bedcec8246e2097dc17a06ebeef22b2610ddc6
Abhilash <abhilashbangera97@gmail.com>
2025-02-28 21:43:46 +0530

    Basics

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
-const heading = React.createElement("h1", {}, "Hello World React!");
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World React!"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, " This is heading 1"),
+    React.createElement("h2", {}, " This is heading 2"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, " This is heading 1"),
+    React.createElement("h2", {}, " This is heading 2"),
+  ]),
+]);
+
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(parent);
