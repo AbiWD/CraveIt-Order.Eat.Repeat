34938fd701568083083346412415b937dd50dce8
Abhilash <abhilashbangera97@gmail.com>
2025-03-05 22:12:28 +0530

    Header & part of restro cards

--- App.js
+++ App.js
@@ -1,30 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://cdn.pixabay.com/photo/2017/02/17/17/33/food-2074638_960_720.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Namaste React!"
-// );
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/11/21/9a379cd4-2bf2-439e-ac06-b1142e1acafa_a532676d-4d23-49c0-8a08-dcb2e93962f6.jpg"
+        alt="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.5 Stars</h4>
+      <h4>20 Minutes</h4>
+    </div>
+  );
+};
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namaste React using jsx
-  </h1>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-// React Functional component
-
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    {/* {Title()} */}
-    <h1 className="heading"> Namste React Functional Component </h1>
-  </div>
-);
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<Applayout />);
