5b4a180bff630fd82545576e8af1e9ca59fae789
Abhilash <abhilashbangera97@gmail.com>
2025-03-16 12:20:30 +0530

    add user comp

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,10 +1,29 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is React Web Series</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Child Constructor");
+  }
+
+  render() {
+    console.log("Child  Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is React Web Series</h2>
+        <User name={"Abhilash (function)"} />
+
+        <UserClass
+          name={"Akshay Saini (class)"}
+          loacation={"Mangalore Class"}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
---
+++ src/Component/User.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const User = ({ name }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="user-card">
+      <h2>Name: {props.name}</h2>
+      <h3>Location: Mangalore</h3>
+      <h4>Contact: abhhilash@gmail.com</h4>
+    </div>
+  );
+};
+
+export default User;
