9eea6680ac58950281e7bc7630f8fe496190d914
Abhilash <abhilashbangera97@gmail.com>
2025-03-19 20:25:11 +0530

    Renamed files and updated project structure

---
+++ CraveIt/src/Component/RestaurantMenu.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+
+const RestaurantMenu = () => {
+  const { resId } = useParams();
+
+  const resInfo = useRestaurantMenu(resId);
+
+  if (resInfo === null) <Shimmer />;
+
+  const { name, cuisines, CostForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(",")} - {CostForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - {item.card.info.price}{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
